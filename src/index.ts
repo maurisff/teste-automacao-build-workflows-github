cc5a52c4f9711c2496ac262f7b2da9c656b78a17
Mauricio <mauricio.sff@gmail.com>
2024-12-21 00:23:11 -0300

    add sufixo -beta na versão do projeto antes de compilar a imagem docker

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { getBuildInfo } from './utils/buildInfo';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const buildInfo = getBuildInfo();
+
+console.log(`Build Info:`);
+console.log(`   Version => ${buildInfo.version}`);
+console.log(`Last Build => ${buildInfo.lastBuild}`);
+console.log('-----------------------------------------------');
 
 // Rota básica que retorna a versão do projeto e a última data de compilação
 app.get('/', (req, res) => {
